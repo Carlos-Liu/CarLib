d18d90e38fb55d73a506ac09bbac3268ccd28167
Carlos-Liu <zliu02@beckman.com>
2017-03-16 13:19:50 +0800

    Add string extension class.
    Compare two strings by ordinal rules.

---
+++ Common.Tests/Extensions/StringExTests.cs
@@ -0,0 +1,123 @@
+﻿using System.Globalization;
+using System.Threading;
+using CarLib.Common.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Common.Tests.Extensions
+{
+    [TestClass]
+    public class StringExTests
+    {
+        [TestMethod]
+        public void EqualOrdinal_BothAreNull_ReturnTrue()
+        {
+            // Arrange
+            string left = null;
+            string right = null;
+
+            // Act
+            bool isEqual = left.EqualOrdinal(right);
+
+            // Assert
+            Assert.IsTrue(isEqual);
+        }
+
+        [TestMethod]
+        public void EqualOrdinal_LeftIsNull_ReturnFalse()
+        {
+            // Arrange
+            string left = null;
+            string right = string.Empty;
+
+            // Act
+            bool isEqual = left.EqualOrdinal(right);
+
+            // Assert
+            Assert.IsFalse(isEqual);
+        }
+
+        [TestMethod]
+        public void EqualOrdinal_LeftIsEmpty_ReturnFalse()
+        {
+            // Arrange
+            string left = string.Empty;
+            string right = null;
+
+            // Act
+            bool isEqual = left.EqualOrdinal(right);
+
+            // Assert
+            Assert.IsFalse(isEqual);
+        }
+
+        [TestMethod]
+        public void EqualOrdinal_LeftAndRightAreTheSame_ReturnTrue()
+        {
+            // Arrange
+            string left = "str";
+            string right = "str";
+
+            // Act
+            bool isEqual = left.EqualOrdinal(right);
+
+            // Assert
+            Assert.IsTrue(isEqual);
+        }
+
+        [TestMethod]
+        public void EqualOrdinal_LeftAndRightAreInDiffCaseButIgnoreCase_ReturnTrue()
+        {
+            // Arrange
+            string left = "str";
+            string right = "STR";
+
+            // Act
+            bool isEqual = left.EqualOrdinal(right);
+
+            // Assert
+            Assert.IsTrue(isEqual);
+        }
+
+        [TestMethod]
+        public void EqualOrdinal_LeftAndRightAreInDiffCaseButCareTheCase_ReturnFalse()
+        {
+            // Arrange
+            string left = "str";
+            string right = "STR";
+
+            // Act
+            bool isEqual = left.EqualOrdinal(right, false);
+
+            // Assert
+            Assert.IsFalse(isEqual);
+        }
+
+        [TestMethod]
+        public void EqualOrdinal_DuplicateTheTurkeyBug_EqualOrdinalSolvesTheProblem()
+        {
+            // There is a issue when comparing the letter 'i' and 'I' in Turkey culture
+            // which is demonstrated as below.
+
+            // Arrange
+            string left = "i";
+            string right = "I";
+            var cultureBackup = Thread.CurrentThread.CurrentCulture;
+
+            // set culture as Turkey, refer to https://msdn.microsoft.com/en-us/library/cc233982.aspx
+            Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("tr-TR");
+
+            // Act
+            bool generalCompareResult = string.Equals("i", "I");
+            // generalCompareResult will be False which should be True, this is the bug
+
+            bool isEqual = left.EqualOrdinal(right);
+
+            // rollback the culture
+            Thread.CurrentThread.CurrentCulture = cultureBackup;
+
+            // Assert
+            Assert.IsTrue(isEqual);
+        }
+    }
+
+}
---
+++ Common/Extensions/StringEx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CarLib.Common.Extensions
+{
+    /// <summary>
+    /// Extension class for the <see cref="string"/> type.
+    /// </summary>
+    public static class StringEx
+    {
+        /// <summary>
+        /// Compare the two strings with ordinal sort rules.
+        /// </summary>
+        /// <param name="left">The left side string to be compared.</param>
+        /// <param name="right">The right side string to be compared.</param>
+        /// <param name="ignoreCase">If ignore case when doing the compare.</param>
+        /// <returns>
+        /// True: If the two strings are equal with specified compare option.
+        /// False: The two strings are not equal with specified compare option.
+        /// </returns>
+        public static bool EqualOrdinal(this string left, string right, bool ignoreCase = true)
+        {
+            if (left == null)
+            {
+                return right == null;
+            }
+
+            if (ignoreCase)
+            {
+                return left.Equals(right, StringComparison.OrdinalIgnoreCase);
+            }
+
+            return left.Equals(right, StringComparison.Ordinal);
+        }
+    }
+}
