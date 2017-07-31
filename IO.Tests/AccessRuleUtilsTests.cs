2500ff3bbd8eb3e1c8882d9d1dfaab0611bbcb2a
Carlos-Liu <zliu02@beckman.com>
2017-07-31 09:56:31 +0800

    Support AccessRuleUtils.SetFullControlForEveryone method.

---
+++ IO.Tests/AccessRuleUtilsTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Security.AccessControl;
+using CarLib.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IO.Tests
+{
+  [TestClass]
+  public class AccessRuleUtilsTests
+  {
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void SetFullControlForEveryone_PathIsNull_ThrowException()
+    {
+      AccessRuleUtils.SetFullControlForEveryone(null);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void SetFullControlForEveryone_PathOnlyIncludesWhitespace_ThrowException()
+    {
+      AccessRuleUtils.SetFullControlForEveryone(" ");
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(DirectoryNotFoundException))]
+    public void SetFullControlForEveryone_PathNotExist_ThrowException()
+    {
+      AccessRuleUtils.SetFullControlForEveryone(@"A:\Path not exist\Path not exist\Path not exist");
+    }
+
+    [TestMethod]
+    public void SetFullControlForEveryone_CreateTempDirectoryThenSetAccessControl_Assigned()
+    {
+      // Arrange
+      var commonDocDir = Environment.GetFolderPath(Environment.SpecialFolder.CommonDocuments);
+      string testDirectory = Path.Combine(commonDocDir, "TestDirectory-A3688960-23CA-47D9-9858-40C7BF8CAC50");
+
+      Directory.CreateDirectory(testDirectory);
+
+      // Act
+      AccessRuleUtils.SetFullControlForEveryone(testDirectory);
+
+      // Assert
+      var directorySecurity = Directory.GetAccessControl(testDirectory, AccessControlSections.Access);
+      var rules = directorySecurity.GetAccessRules(true, true, typeof(System.Security.Principal.NTAccount));
+
+      foreach (FileSystemAccessRule rule in rules)
+      {
+        Assert.AreEqual("Everyone", rule.IdentityReference.Value);
+        Assert.AreEqual(FileSystemRights.Modify | FileSystemRights.Synchronize, rule.FileSystemRights);
+        Assert.AreEqual(PropagationFlags.None, rule.PropagationFlags);
+        Assert.AreEqual(InheritanceFlags.ContainerInherit | InheritanceFlags.ObjectInherit, rule.InheritanceFlags);
+        Assert.AreEqual(AccessControlType.Allow, rule.AccessControlType);
+
+        break;
+      }
+
+      Directory.Delete(testDirectory);
+    }
+  }
+}
---
+++ IO/AccessRuleUtils.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Security.AccessControl;
+using System.Security.Principal;
+
+namespace CarLib.IO
+{
+    /// <summary>
+    /// Utilities class to adding/removing access rule for specified directory.
+    /// </summary>
+    public static class AccessRuleUtils
+    {
+        /// <summary>
+        /// Set full control access rule for Everyone on specified directory.
+        /// </summary>
+        /// <param name="targetDirectory">The directory to set access rule.</param>
+        /// <exception cref="ArgumentException">Occurred when the directory argument is invalid.</exception>
+        /// <exception cref="DirectoryNotFoundException">Occurred when the directory does not exist on disk.</exception>
+        public static void SetFullControlForEveryone(string targetDirectory)
+        {
+            if (string.IsNullOrWhiteSpace(targetDirectory))
+            {
+                throw new ArgumentException("The target directory is null or whitespace only.");
+            }
+
+            if (!Directory.Exists(targetDirectory))
+            {
+                throw new DirectoryNotFoundException(
+                    string.Format("Directory '{0}' not found on disk.", targetDirectory));
+            }
+
+            DirectorySecurity directorySecurity = Directory.GetAccessControl(targetDirectory);
+
+            // Using this instead of the "Everyone" string means we work on non-English systems.
+            SecurityIdentifier everyone = new SecurityIdentifier(WellKnownSidType.WorldSid, null);
+            var accessRule = new FileSystemAccessRule(
+                everyone,
+                FileSystemRights.Modify | FileSystemRights.Synchronize,
+                InheritanceFlags.ContainerInherit | InheritanceFlags.ObjectInherit, 
+                PropagationFlags.None,
+                AccessControlType.Allow);
+
+            directorySecurity.AddAccessRule(accessRule);
+
+            Directory.SetAccessControl(targetDirectory, directorySecurity);
+        }
+    }
+}
