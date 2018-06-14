0ed178ead390b46b77b712cdbfc2a037a46c8f2c
Carlos-Liu <zliu02@beckman.com>
2018-06-14 18:21:41 +0800

    Add DbUtils to escape LIKE clause w/o unit tests.

---
+++ Common/Database/DbUtils.cs
@@ -0,0 +1,22 @@
+﻿namespace CarLib.Common.Database
+{
+  public static class DbUtils
+  {
+    // Refer to
+    // https://stackoverflow.com/questions/439495/how-can-i-escape-square-brackets-in-a-like-clause
+    // https://docs.microsoft.com/en-us/sql/t-sql/language-elements/like-transact-sql?view=sql-server-2017#pattern-matching-with-the-escape-clause
+    /// <summary>
+    /// Escapes the LIKE clause for SQL.
+    /// </summary>
+    /// <param name="param">The parameter.</param>
+    /// <returns>The clause string which is escaped for SQL query.</returns>
+    public static string EscapeForLikeSql(string param)
+    {
+      if (string.IsNullOrEmpty(param)) return null;
+
+      return param.Replace("[", "[[]")
+                  .Replace("%", "[%]")
+                  .Replace("_", "[_]");
+    }
+  }
+}
