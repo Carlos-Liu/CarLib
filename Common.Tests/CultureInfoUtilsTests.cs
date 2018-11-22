eae147afda5364225854d0675338c2c4feaa2e6f
Carlos-Liu <zliu02@beckman.com>
2018-11-22 14:58:58 +0800

    Add culture info utility to check if a specified culture is valid or not.

---
+++ Common.Tests/CultureInfoUtilsTests.cs
@@ -0,0 +1,53 @@
+﻿using CarLib.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Common.Tests
+{
+  [TestClass]
+  public class CultureInfoUtilsTests
+  {
+    [TestMethod]
+    public void IsLanguageAndRegionValid_OneValidWord_ReturnTrue()
+    {
+      // ar means Arabic
+      var isValid = CultureInfoUtils.IsLanguageAndRegionValid("ar");
+      Assert.IsTrue(isValid);
+    }
+
+    [TestMethod]
+    public void IsLanguageAndRegionValid_ThreeValidWords_ReturnTrue()
+    {
+      // Azerbaijani (Cyrillic)
+      var isValid = CultureInfoUtils.IsLanguageAndRegionValid("az-Cyrl-AZ");
+      Assert.IsTrue(isValid);
+    }
+
+    [TestMethod]
+    public void IsLanguageAndRegionValid_LowerCaseButValid_ReturnTrue()
+    {
+      var isValid = CultureInfoUtils.IsLanguageAndRegionValid("en-us");
+      Assert.IsTrue(isValid);
+    }
+
+    [TestMethod]
+    public void IsLanguageAndRegionValid_UpperCaseButValid_ReturnTrue()
+    {
+      var isValid = CultureInfoUtils.IsLanguageAndRegionValid("EN-US");
+      Assert.IsTrue(isValid);
+    }
+
+    [TestMethod]
+    public void IsLanguageAndRegionValid_ChineseMailandCulture_ReturnTrue()
+    {
+      var isValid = CultureInfoUtils.IsLanguageAndRegionValid("zh-CN");
+      Assert.IsTrue(isValid);
+    }
+
+    [TestMethod]
+    public void IsLanguageAndRegionValid_Invalid_ReturnFalse()
+    {
+      var isValid = CultureInfoUtils.IsLanguageAndRegionValid("en-CN");
+      Assert.IsFalse(isValid);
+    }
+  }
+}
---
+++ Common/CultureInfoUtils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+namespace CarLib.Common
+{
+  public static class CultureInfoUtils
+  {
+    /// <summary>
+    /// Determines whether the specified language and region is valid.
+    /// </summary>
+    /// <param name="languageAndRegion">The language and region.</param>
+    /// <returns>
+    ///   <c>true</c> if the specified language and region is valid; otherwise, <c>false</c>.
+    /// </returns>
+    public static bool IsLanguageAndRegionValid(string languageAndRegion)
+    {
+      // for full list please refer to https://msdn.microsoft.com/en-us/library/cc233982.aspx
+      return CultureInfo.GetCultures(CultureTypes.AllCultures)
+        .Any(culture => string.Equals(culture.Name, languageAndRegion, StringComparison.CurrentCultureIgnoreCase));
+    }
+  }
+}
