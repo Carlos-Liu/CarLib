fa7fbd2910682206d364436cc6cc794ebfb764c6
Carlos Liu <Carlos-Liu@users.noreply.github.com>
2026-01-13 10:20:09 +0800

    Update StringEx.cs by adding CompareInvariantIgnoreCase

--- Common/Extensions/StringEx.cs
+++ Common/Extensions/StringEx.cs
@@ -31,5 +31,23 @@ namespace CarLib.Common.Extensions
 
             return left.Equals(right, StringComparison.Ordinal);
         }
+
+        /// <summary>
+        /// Compare two strings using InvariantCulture and ignoring case.
+        /// </summary>
+        /// <param name="str">The string to compare.</param>
+        /// <param name="other">The other string to compare.</param>
+        /// <returns>Return True if the two strings are equal, else return False.</returns>
+        public static bool CompareInvariantIgnoreCase(this string str, string other)
+        {
+            // Why use ToUpperInvariant instead of string.Equals with StringComparison.InvariantCultureIgnoreCase?
+            //  -The method will be used by EF core to SQL translation, so string.Equals with StringComparison.InvariantCultureIgnoreCase does not work.
+            // Why use ToUpperInvariant instead of ToUpper?
+            //  -ToUpper comparision does not work for Turkish culture.
+            //  -Reference: https://stackoverflow.com/questions/3550213/in-c-sharp-what-is-the-difference-between-toupper-and-toupperinvariant
+            return (str == null && other == null) ||
+                   (str != null && other != null &&
+                   str.ToUpperInvariant() == other.ToUpperInvariant());
+        }
     }
 }
